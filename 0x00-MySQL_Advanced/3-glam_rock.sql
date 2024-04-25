6a677477ccaf68f25eaf7655abb5ad9c68374f8a
Josephdada01 <ridwandada500@gmail.com>
2024-04-25 11:27:45 +0000

    database table

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -6,7 +6,7 @@
 -- (in years until 2022 - please use 2022 instead of YEAR(CURDATE()))
 -- You should use attributes formed and split for computing the lifespan
 -- Your script can be executed on any database
-SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE 'Glam rock'
 ORDER BY lifespan DESC;
