13337eca74d6d80accb08588c2e425bf53d220ae
Josephdada01 <ridwandada500@gmail.com>
2024-05-03 13:01:34 +0100

    ammend the style

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -12,5 +12,6 @@ def update_topics(mongo_collection, name, topics):
     topics (list of strings) will be the list of topics approached
     in the school
     """
-    result = mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    result = mongo_collection.update_many({"name": name},
+                                          {"$set": {"topics": topics}})
     return result.modified_count
